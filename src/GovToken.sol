2e584b6fcc246028e2af0c81d509db06f5aba311
Akhil Potla <akhil4400@gmail.com>
2025-04-29 21:48:27 -0600

    Inheriting from ERC20Capped class

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -2,22 +2,29 @@
 pragma solidity ^0.8.29;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
-contract GovToken is ERC20, ERC20Permit, ERC20Votes {
+contract GovToken is ERC20Capped, ERC20Permit, ERC20Votes {
     constructor(
-        uint256 initialSupply
-    ) ERC20("Good Morning Vault", "GMV") ERC20Permit("Good Morning Vault") {
+        uint256 initialSupply,
+        uint256 cap
+    )
+        ERC20("Good Morning Vault", "GMV")
+        ERC20Capped(cap)
+        ERC20Permit("Good Morning Vault")
+    {
         _mint(msg.sender, initialSupply);
+        _delegate(msg.sender, msg.sender);
     }
 
     function _update(
         address from,
         address to,
         uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
+    ) internal override(ERC20, ERC20Capped, ERC20Votes) {
         super._update(from, to, amount);
     }
 
