12cee60c3b8d05cfbfa020b6d8fce3b7f35d6390
Akhil Potla <akhil4400@gmail.com>
2025-05-10 14:47:39 -0600

    Adding minting public function

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -6,14 +6,24 @@ import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20C
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract GovToken is ERC20Capped, ERC20Permit, ERC20Votes {
+contract GovToken is ERC20Capped, ERC20Permit, ERC20Votes, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     constructor(
         uint256 initialSupply,
         uint256 cap
     ) ERC20("GovToken", "GT") ERC20Capped(cap) ERC20Permit("GovToken") {
         _mint(msg.sender, initialSupply);
         _delegate(msg.sender, msg.sender);
+
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
     }
 
     function _update(
